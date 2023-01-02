544d16d91d3ca9cbcc78d2c5acd053be9f9fb15f
Danilo Campanhari <danilocampanhari64@gmail.com>
2023-01-02 14:38:00 +0100

    index banner ok

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [setIndex] = useState(1);
+  const [index, setIndex] = useState(1);
   const toRotate = [ "Benvenuto", "Ti ringraziamo per essere qui", "Sii il benvenuto" ];
   const period = 2000;
 
